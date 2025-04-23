7aaeda3eff8dbc1a566864a45aa3cbf29fc69b44
Sagnick Manna <142563314+sagnick11tib@users.noreply.github.com>
2025-04-24 02:20:16 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,13 @@
+def call(String ImageName, String ImageTag, String dockerhubuser){
+  echo "This is pushing the image to DockerHub"
+                withCredentials([
+                    usernamePassword('credentialsId':"dockerHubCred",
+                                      passwordVariable:"dockerHubPass",
+                                      usernameVariable:"dockerHubUser")
+                                ])
+                {
+                sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+                sh "docker image tag devopslab-app:latest ${dockerHubUser}/${ImageName}:${ImageTag}"
+                sh "docker push ${dockerHubUser}/${ImageName}:${ImageTag}"
+                }
+}
