68f6987c883df0fa297c8d416cfa97fa955ae430
Sagnick Manna <142563314+sagnick11tib@users.noreply.github.com>
2025-04-24 02:28:57 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,5 @@
 def call(){
    echo "This is deploying the Code using script"
    //sh "docker run -d -p 3000:3000 devopslab-app:latest"
-   sh "docker-compose up -d"
+   sh "docker-compose down && docker-compose up -d"
 }
