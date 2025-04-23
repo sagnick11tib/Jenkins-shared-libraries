3214ea270f3b76c307a57bc22bfb8d884c63266b
Sagnick Manna <142563314+sagnick11tib@users.noreply.github.com>
2025-04-24 02:48:57 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -8,7 +8,7 @@ def call(String ImageName, String ImageTag, String dockerhubuser){
                                 ])
                 {
                 sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-                sh "docker image tag devopslab-app:latest ${dockerHubUser}/${ImageName}:${ImageTag}"
+                //sh "docker image tag devopslab-app:latest ${dockerHubUser}/${ImageName}:${ImageTag}"
                 sh "docker push ${dockerHubUser}/${ImageName}:${ImageTag}"
                 }
 }
