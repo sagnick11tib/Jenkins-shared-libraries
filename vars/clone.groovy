e4df5c50e3164cf84faacdd7497c27afc4be1679
Sagnick Manna <142563314+sagnick11tib@users.noreply.github.com>
2025-04-24 01:56:51 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String url, String branch){
-   echo "This is cloning the Code"
+   echo "This is cloning the Code Using Github Jenkins Script"
    git url: "${url}", branch: "${branch}"
    echo "Code cloning Succesfully"
 }
